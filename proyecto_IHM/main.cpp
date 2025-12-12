23a24fc18c7b2f4601119b1c112d6898d0b4646f
Nuria Urrecho <nurrtor@etsinf.upv.es>
2025-12-12 12:12:04 +0100

    Nueva hoja de estilos

--- proyecto_IHM/main.cpp
+++ proyecto_IHM/main.cpp
@@ -1,10 +1,22 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Load and apply stylesheet
+    QFile file(":/resources/stylesheet.qss");
+    if(file.open(QFile::ReadOnly | QFile::Text))
+    {
+        QTextStream stream(&file);
+        a.setStyleSheet(stream.readAll());
+        file.close();
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
